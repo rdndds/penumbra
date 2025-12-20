3c70d2b09f8aca3d08a20ab06cbf1b8a5376aea5
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-12-20 15:08:09 +0100

    tui: Add Card component
    This component allows defining small cards for showing info, in a row.

--- tui/src/components/mod.rs
+++ tui/src/components/mod.rs
@@ -1,8 +1,12 @@
+pub mod card_view;
 pub mod description_menu;
 pub mod dialog;
 pub mod file_explorer;
 pub mod progress_bar;
 pub mod selectable_list;
+// Re-exports :D
+
+pub use card_view::{Card, CardRow};
 pub use description_menu::{DescriptionMenu, DescriptionMenuItem};
 pub use dialog::{DialogBuilder, DialogButton};
 pub use file_explorer::{ExplorerResult, FileExplorer};
